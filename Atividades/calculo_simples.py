807ddf4ab142ecb1f7c5b52499b45028c9f87227
Gabriel Gomes da Silva <gabriel.gomes221@edu.unifil.br>
2022-08-16 14:52:45 -0300

    atividade calculo simples

---
+++ Atividades/calculo_simples.py
@@ -0,0 +1,11 @@
+codigo, unidades, preco = input().split(" ")
+codigo1, unidades1, preco1 = input().split(" ")
+
+codigo = int(codigo)
+unidades = int(unidades)
+preco = float(preco)
+codigo1 = int(codigo)
+unidades1 = int(unidades)
+preco1 = float(preco)
+
+

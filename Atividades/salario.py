2a6a05963ad1ed154af003b298aac234ef9e746c
[gabrielgomestardust] <[gabriel.gomes221@edu.unifil.br]>
2022-08-15 22:02:21 -0300

    Exercicios aula do Mario

---
+++ Atividades/salario.py
@@ -0,0 +1,11 @@
+from tokenize import Double
+
+
+numero = int(input())
+horas = int(input())
+salario = float(input())
+
+R = salario * horas
+
+print(f'NUMBER = {numero}')
+print(f'SALARY = U$ {')

c537b34d5f5420e413d836b3aa539ccd3549fbc2
[gabrielgomestardust] <[gabriel.gomes221@edu.unifil.br]>
2022-08-15 22:03:40 -0300

    exercicios do mario

---
+++ Atividades/area_do_circulo.py
@@ -0,0 +1,8 @@
+n = 3.14159
+r = float(input())
+
+a = n * (r*r)
+
+print("A=%0.4f"%a)
+
+
---
+++ Atividades/media2.py
@@ -0,0 +1,10 @@
+A = float(input())
+B = float(input())
+C = float(input())
+A = (A*2)
+B = (B*3)
+C = (C*5)
+
+R = (A+B+C)/10
+
+print("MEDIA = %0.1f"%R)
